dd57ba050ad88d4a6ee5750956bcf7cdc0461e9a
Corey Porter <coreyaporter@gmail.com>
2023-12-01 21:37:55 -0800

    day2

--- README.md
+++ README.md
@@ -9,3 +9,4 @@ For any day's problem, it's probably
     ./dayN < input.txt
 
 * [Day 1](day1/source/app.d). Fun little problem. Probably could have done more with ranges.
+* [Day 2](day2/source/app.d). Need to spend a little more time and not just write Python in D.
---
+++ day2/source/app.d
@@ -0,0 +1,48 @@
+import std.stdio;
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.string;
+import std.range;
+
+enum int[string] MAXIMUM = [
+	"red": 12,
+	"green": 13,
+	"blue": 14
+];
+
+void main()
+{
+	auto lines = stdin.byLine.map!(to!string).map!chomp.array;
+	int p1 = 0;
+	int p2 = 0;
+
+	foreach (i, line; lines.enumerate(1)) {
+		auto groups = line.split(":")[1].split(";");
+		bool bad = false;
+		int[string] local_max = [
+			"red": 0,
+			"green": 0,
+			"blue": 0
+		];
+		foreach (group; groups) {
+			auto colors = group.split(',');
+			foreach (color; colors) {
+				auto a = color.split();
+				auto n = a.front.to!int;
+				auto col = a.back.chomp;
+				if (n > MAXIMUM[col]) {
+					bad = true;
+				}
+				local_max[col] = max(local_max[col], n);
+			}
+		}
+		if (! bad) {
+			p1 += i;
+		}
+		p2 += reduce!((a, b) => a * b)(1, local_max.values);
+	}
+
+	writefln("p1: %d", p1);
+	writefln("p2: %d", p2);
+}
